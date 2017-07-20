6d128b8805e8015a108a8ad706f4f7a3db14944a
nakagat@gmail.com <2wsxzaq1>
2017-07-21 08:57:54 +0900

    Initial commit

---
+++ app/src/main/java/net/kuex3/scbw2/Scbw2Widget.java
@@ -0,0 +1,16 @@
+package net.kuex3.scbw2;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class Scbw2Widget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    }
+}
+
