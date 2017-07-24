e0f0a9ae511b32c6d5f7d29bfa1a8539d562463d
nakagat@gmail.com <2wsxzaq1>
2017-07-24 10:32:20 +0900

    update

--- app/src/main/java/net/kuex3/scbw2/Scbw2Receiver.java
+++ app/src/main/java/net/kuex3/scbw2/Scbw2Receiver.java
@@ -3,6 +3,8 @@ package net.kuex3.scbw2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.util.Log;
 
 public class Scbw2Receiver extends BroadcastReceiver {
@@ -15,11 +17,16 @@ public class Scbw2Receiver extends BroadcastReceiver {
 
     }
 
-    public static final Scbw2Receiver getInstance() {
+    public static void init(Context context) {
         if (receiver == null) {
+            Log.d(Scbw2Widget.TAG, "new Scbw2Receiver");
             receiver = new Scbw2Receiver();
+
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+            Intent intent = context.registerReceiver(receiver, filter);
+            receiver.onReceive(context, intent);
         }
-        return receiver;
     }
 
     @Override
@@ -29,9 +36,9 @@ public class Scbw2Receiver extends BroadcastReceiver {
         if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
             Intent updateIntent = new Intent(context, Scbw2Widget.class);
             updateIntent.setAction(Scbw2Widget.ACTION_UPDATE);
-            int scale = intent.getIntExtra("scale", 0);
-            int level = intent.getIntExtra("level", -1);
-            int plugged = intent.getIntExtra("plugged", 0);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
             String battery = "";
             if (scale > 0 && level > -1) {
                 int per = (level * 100) / scale;
